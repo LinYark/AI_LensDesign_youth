7a950da1e131e22487e8ff9423def094bdc00f48
Sanhexiaobai <1369684818@qq.com>
2024-01-30 09:20:55 +0800

    getline

--- test.cpp
+++ test.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
+
 int main()
 {
-    cout << "hello world" << endl;
+    ifstream input("a.txt");
+    // string word;
+    // while (input >> word)
+    // {
+    //     cout << word << endl;
+    // }
+    // ifstream input("a.txt");
+    string line;
+    while (getline(input, line))
+    {
+        cout << line << endl;
+    }
     return 0;
 }
