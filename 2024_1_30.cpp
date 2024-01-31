13686d664ae29f3ddc534cb588a883fafcd013f6
Sanhexiaobai <1369684818@qq.com>
2024-01-31 19:39:59 +0800

    .

--- 2024_1_30.cpp
+++ 2024_1_30.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     const int n = 8;
-    int arr[n] = {2, 7, 5, 1, 9, 4, 7, 8};
+    int arr[n] = {3, 7, 5, 1, 9, 4, 7, 8};
 
     int newArr[n];
     for (int i = 0; i < n; i++)
